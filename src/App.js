849581609821082fc4cc8d480634d8e66d218fc7
Deepak Raj <66163072+deepakraj-18@users.noreply.github.com>
2025-09-08 18:06:55 +0530

    pages created

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
-import Welcome from "./components/Welcome";
-import Specialities from "./components/Specialities";
-import Doctors from "./components/Doctors";
-import CareSpecialties from "./Carespecialist";
-import Events from "./components/Events";
-import Beyond from "./components/Beyond";
-import Testimonial from "./components/Testimonial";
+import Hero from "./pages/Hero";
 import Footer from "./components/Footer";
-import Blog from "./components/Blog";
-import Surgical from "./components/Surgical";
 
 function App() {
   return (
     <div className="font-sans">
       <Navbar />
       <Hero />
-      <Card />
-      <Welcome />
-      <Specialities />
-      <Doctors />
-      <CareSpecialties /> 
-      <Surgical />
-      <Beyond />
-      <Events />
-      <Blog />
-      <Testimonial />
       <Footer />
     </div>
   );
